0a990a50ead61df2dd95bd6dbb70109e3da98573
Niya-fathima <fniya186@gmail.com>
2023-10-28 15:48:20 +0530

    initial

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+void main()
+{
+runApp(MaterialApp(home: Center(child:Text("haiii",style:TextStyle(fontSize: 40,color: Colors.white,decoration: TextDecoration.none) ,))));
+}
